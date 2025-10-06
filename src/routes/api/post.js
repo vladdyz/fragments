f921635c5444c2b623b09d3edc0baa18350aaa86
Vladislav Zolotukhin <vzolotukhin@myseneca.ca>
2025-10-06 15:59:43 -0400

    Implemented fragments POST route and unit testing

---
+++ src/routes/api/post.js
@@ -0,0 +1,58 @@
+const logger = require('../../logger');
+const { Fragment } = require('../../model/fragment');
+
+module.exports = async (req, res) => {
+  try {
+    if (Buffer.isBuffer(req.body)) {
+      logger.debug('POST request from authenticated user');
+      // confirm user is authenticated
+      const authenticated = req.user;
+      if (!authenticated) {
+        logger.warn(`User ${req.user} is not authenticated!`);
+        return res.status(401).json({ error: 'Unauthenticated User' });
+      }
+      // if user is authenticated and fragment type is supported, lets save it
+      logger.debug('Creating Fragment instance');
+      const fragment = new Fragment({
+        ownerId: req.user, // this needs to be req.user, not req.body.user (req.body is the Buffer!)
+        type: req.get('Content-Type'),
+        size: req.body.length,
+      });
+      await fragment.save();
+      // store the buffer
+      await fragment.setData(req.body);
+      logger.debug(`Fragment data: ${fragment}`);
+      logger.debug('Fragment saved and buffer stored');
+      // configure URL depending on if we're running locally or not then set to location header
+      const apiUrl = process.env.API_URL || `http://${req.headers.host}`;
+      res.set('Location', `${apiUrl}/v1/fragments/${fragment.id}`);
+      /* Example of what a response should look like according to the documentation:
+      {
+        "status": "ok",
+        "fragment": {
+          "id": "30a84843-0cd4-4975-95ba-b96112aea189",
+          "ownerId": "11d4c22e42c8f61feaba154683dea407b101cfd90987dda9e342843263ca420a",
+          "created": "2021-11-02T15:09:50.403Z",
+          "updated": "2021-11-02T15:09:50.403Z",
+          "type": "text/plain",
+          "size": 256
+        }
+      } 
+      */
+      res.status(200).json({
+        status: 'ok',
+        fragment,
+      });
+      logger.info('POST request successful: new fragment posted');
+    } else {
+      logger.warn('Invalid unsupported type');
+      res.status(400).json({
+        status: 'error',
+        fragments: [],
+        message: 'Invalid unsupported type',
+      });
+    }
+  } catch (err) {
+    logger.error(`Unexpected error occurred (you shouldn't be seeing this!): ${err}`);
+  }
+};
---
+++ tests/unit/post.test.js
@@ -0,0 +1,80 @@
+const { Fragment } = require('../../src/model/fragment');
+const app = require('../../src/app');
+const request = require('supertest');
+
+describe('POST /fragments tests', () => {
+  test('Unauthenticated users rejected', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .set('Content-Type', 'text/plain')
+      .send('No auth test');
+    expect(res.statusCode).toBe(401);
+  });
+
+  test('Successfully post a text fragment', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('testaccount1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('text');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe('ok');
+  });
+
+  test('Unsupported content type is rejected', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('testaccount1@email.com', 'password1')
+      .set('Content-Type', 'application/exe')
+      .send('test');
+    expect(res.statusCode).toBe(400);
+    expect(res.body.message).toBe('Invalid unsupported type');
+  });
+
+  test('Response includes all properties (id, ownerId, created, updated, size, type) with correct values', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('testaccount1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('This text has 27 characters');
+    const fragment = res.body.fragment;
+    expect(fragment).toHaveProperty('id');
+    expect(fragment).toHaveProperty('ownerId', 'testaccount1@email.com');
+    expect(fragment).toHaveProperty('type', 'text/plain');
+    expect(fragment).toHaveProperty('size', 27);
+    expect(fragment).toHaveProperty('created');
+    expect(fragment).toHaveProperty('updated');
+  });
+
+  test('POST response includes a Location header with a full URL to GET the created fragment', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('testaccount1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('I love coffee');
+    const fragment = res.body.fragment;
+    expect(res.headers.location).toMatch(new RegExp(`/v1/fragments/${fragment.id}$`));
+  });
+
+  test('Confirm that a fragment was actually posted', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('testaccount1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('Hello World');
+    const fragment = res.body.fragment;
+    const isSaved = await Fragment.byId(fragment.ownerId, fragment.id);
+    expect(isSaved).toBeDefined();
+    expect(isSaved.isText).toBe(true);
+    expect(isSaved.size).toBe(11);
+  });
+
+  test('Invalid users rejected', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('incog@email.com', 'nito')
+      .set('Content-Type', 'text/plain')
+      .send('No auth test');
+    expect(res.statusCode).toBe(401);
+  });
+});
