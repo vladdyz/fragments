7a7500d985a460f03d80ce613bc4f46c86a51021
Vladislav Zolotukhin <vzolotukhin@myseneca.ca>
2025-10-26 17:38:55 -0400

    Implemented path parameters to fragments and unit tests, debugged unexpected return types in routes

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
+const { deleteFragment } = require('../../src/model/data/memory/index');
+
 describe('GET /v1/fragments', () => {
   // If the request is missing the Authorization header, it should be forbidden
   test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(401));
@@ -59,5 +61,26 @@ describe('GET /v1/fragments/:id', () => {
       .get(`/v1/fragments/${fragment.id}`)
       .auth('invalidaccount@email.com', '123');
     expect(req.statusCode).toBe(401);
+  });
+  test('Attempting to return a nonexistent fragment by an authenticated user should return 404 Not Found', async () => {
+    const missing = 'notARealFragment';
+    const res = await request(app)
+      .get(`/v1/fragments/${missing}`)
+      .auth('kittens@email.com', 'drowssap');
+    expect(res.statusCode).toBe(404);
+  });
+  test('Attempting to return a deleted fragment by an authenticated user should return 404 Not Found', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('testaccount1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('I love coffee');
+    const fragment = res.body.fragment;
+    // commenting this out returns 200 instead of 404, supporting the actual removal of the fragment
+    await deleteFragment(fragment.ownerId, fragment.id);
+    const req = await request(app)
+      .get(`/v1/fragments/${fragment.id}`)
+      .auth('testaccount1@email.com', 'password1');
+    expect(req.statusCode).toBe(404);
   });
 });
