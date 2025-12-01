4c7808cf4e3bba3822e4c8bd03c8527cab4af652
Vladislav Zolotukhin <vzolotukhin@myseneca.ca>
2025-12-01 03:27:07 -0500

    Correcting misplaced line of code in AWS

--- src/model/data/aws/index.js
+++ src/model/data/aws/index.js
@@ -1,6 +1,5 @@
 // If the environment sets an AWS Region, we'll use AWS backend
 // services (S3, DynamoDB); otherwise, we'll use an in-memory db.
-module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
 const s3Client = require('./s3Client');
 const { PutObjectCommand, GetObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
 const logger = require('../../../logger');
